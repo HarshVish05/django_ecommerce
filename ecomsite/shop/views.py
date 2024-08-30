d4c4a0b27bc567c576cd973f1745b22dc3abddd4
Harsh Vishwakarma <Harsh.Vishwakarma@gds.ey.com>
2024-08-30 17:29:11 +0530

    feat: added checkout functionality

--- ecomsite/shop/views.py
+++ ecomsite/shop/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Products
+from .models import Products, Order
 from django.core.paginator import Paginator
 # Create your views here.
 
@@ -28,4 +28,16 @@ def detail(request, id):
 
 
 def checkout(request):
+    if request.method == "POST":
+        items = request.POST.get("items", '')
+        name = request.POST.get("name", "")     # (nameOfElement, defaultValue)
+        email = request.POST.get("email", "")
+        address = request.POST.get("address", "")
+        city = request.POST.get("city", "")
+        state = request.POST.get("state", "")
+        zipcode = request.POST.get("zipcode", "")
+        
+        order = Order(items= items, name= name, email= email, address= address, city= city, state=state, zipcode= zipcode)
+        order.save()
+
     return render(request, 'shop/checkout.html')
